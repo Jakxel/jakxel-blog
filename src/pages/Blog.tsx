7db428b1f8ba0e57954d42affa122b4eb3551bfd
Victor Jakxel Islas Carreon <jakxel20@gmail.com>
2025-07-31 18:49:13 -0700

    Blog feature

--- src/pages/Blog.tsx
+++ src/pages/Blog.tsx
@@ -1,8 +1,20 @@
-const Blog = () =>{
+import '../styles/pages/Blog.css'
+
+const Blog = () => {
   return (
-    <div>
-    
+    <div className="box">
+      <div className="blog-title-section">
+        <div className="blog-title-info">
+          <h1>Blog Post Section</h1>
+          <p>
+            This section is dedicated to my blog posts, which may cover a variety of topics related to technology, programming, research on different articles or publications from other sources, or simply spontaneous thoughts that come to mind.
+          </p>
+        </div>
+      </div>
     </div>
-  )
-}
+  );
+};
+
 export default Blog;
+
+
---
+++ src/posts/post-1.md
@@ -0,0 +1,14 @@
+# My First Blog Post
+
+This is the content of my first blog post written in Markdown.
+
+## Introduction
+
+Markdown is a lightweight markup language with plain-text formatting syntax.
+
+### Key Features
+
+- Easy to write
+- Easy to read
+
+Enjoy writing your blogs!
